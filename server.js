de5a3eca3c347be220f33ed1e15051b55b146568
keithknox <keithwknox@gmail.com>
2021-07-19 23:28:04 -0400

    added Contact & About pages and basic routing.

--- server.js
+++ server.js
@@ -7,5 +7,12 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'))
 });
 
+app.get('/about', function(req, res) {
+    res.sendFile(path.join(__dirname, '/about.html'))
+});
+
+app.get('/contact', function(req, res) {
+    res.sendFile(path.join(__dirname, '/contact.html'))
+});
 app.listen(port)
 console.log(`App started on localhost:${port}`)
